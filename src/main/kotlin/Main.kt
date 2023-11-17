ee0a5e8528e12a87a7553254cac4919d721a066c
guiBrisson <guibrisson@hotmail.com>
2023-11-17 14:55:55 -0300

    Add app theme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,20 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import presentation.designsystem.components.ThemeSwitch
+import presentation.designsystem.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    AppTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            ThemeSwitch(isExpanded = true)
         }
     }
 }
---
+++ src/main/kotlin/presentation/designsystem/theme/Colors.kt
@@ -0,0 +1,25 @@
+package presentation.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val light_primary = Color(0xFF0084FF)
+val light_onPrimary = Color(0xFFFFFFFF)
+val light_secondary = Color(0xFF006A63)
+val light_onSecondary = Color(0xFFFFFFFF)
+val light_error = Color(0xFFBA1A1A)
+val light_onError = Color(0xFFFFFFFF)
+val light_background = Color(0xFFFEFEFE)
+val light_onBackground = Color(0xFF191C1D)
+val light_surface = Color(0xFFf3f4f4)
+val light_onSurface = Color(0xFF191C1D)
+
+val dark_primary = Color(0xFF0084FF)
+val dark_onPrimary = Color(0xFFFFFFFF)
+val dark_secondary = Color(0xFF153d37)
+val dark_onSecondary = Color(0xFFFFFFFF)
+val dark_error = Color(0xFFED4A4A)
+val dark_onError = Color(0xFF690005)
+val dark_background = Color(0xFF1C1C1C)
+val dark_onBackground = Color(0xFFFFFFFF)
+val dark_surface = Color(0xFF161616)
+val dark_onSurface = Color(0xFFFFFFFF)
